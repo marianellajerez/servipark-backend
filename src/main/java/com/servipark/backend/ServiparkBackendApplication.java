341a54bf22a35f63215f0746ffe8c940f5163e0e
Marianella Jerez <marianella.08@hotmail.com>
2025-10-27 22:45:49 -0500

    Inicia la construcción del backend para ServiPark

---
+++ src/main/java/com/servipark/backend/ServiparkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.servipark.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiparkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiparkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servipark/servipark_backend/ServiparkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servipark.servipark_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiparkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
