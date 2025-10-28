78e97a3fe03831a2cdf744bdacd1b27d776ed70c
Marianella Jerez <marianella.08@hotmail.com>
2025-10-28 00:37:48 -0500

    Habilita la auditoría para JPA y crea la clase auditable

--- src/main/java/com/servipark/backend/ServiparkBackendApplication.java
+++ src/main/java/com/servipark/backend/ServiparkBackendApplication.java
@@ -2,8 +2,10 @@ package com.servipark.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ServiparkBackendApplication {
 
 	public static void main(String[] args) {
